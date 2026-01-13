db07365b33004b9e71940db9d1d36f3ec4d71dc2
rawrjaaa” <162077143+rawrjaaa@users.noreply.github.com>
2026-01-13 11:22:17 +0700

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@ import './App.css'
 function App() {
   let nama = 'raja'
   return (
-    <>
+    <> {/* Fragment */}
       <div>
         <h1>Hello, World!</h1>
         <h2>Selamat Datang {nama}</h2>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas corrupti doloremque ipsum asperiores voluptatum qui illo, magni nobis, blanditiis velit, molestiae enim ullam? Corporis itaque praesentium quisquam voluptatum vel soluta?</p>
+        <img src="/src/assets/react.svg" alt="react" />
       </div>
     </>
   )
 }
 
 export default App
+
+/*
+Aturan Penggunaan JSX
+1. Hanya bisa return 1 elemen -> harus dibungkus dalam react Fragment <></>
+2. Semua tag harus ditutup
+3. Penggunaan camelCase untuk atribut
+4. JSX harus di-return untuk dapat ditampilkan
+*/
