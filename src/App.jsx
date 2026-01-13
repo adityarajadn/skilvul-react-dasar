9160bd1c4b7149edd5e2a160522e754fb2b96aae
rawrjaaa” <162077143+rawrjaaa@users.noreply.github.com>
2026-01-13 11:18:00 +0700

    jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let nama = 'raja'
+  return (
+    <>
+      <div>
+        <h1>Hello, World!</h1>
+        <h2>Selamat Datang {nama}</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
